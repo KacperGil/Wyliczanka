8e2fa9ba8acb713af5b5ab5e0e7af5ec56f79bd2
KacperGil <kacpergil1610@gmail.com>
2022-11-24 10:18:25 +0100

    nowa wyliczanka fori

--- src/Wyliczanka.java
+++ src/Wyliczanka.java
@@ -1,7 +1,13 @@
 public class Wyliczanka {
     public static void main(String[] args) {
-        for(int i=1; i<=10; i++){
-            System.out.println(i);
+        int liczydlo=0;
+        for(int i=1; i<=9; i++){
+            System.out.print(i+" ");
+            liczydlo++;
+            if (liczydlo==3){
+                System.out.println("\n");
+                liczydlo=0;
+            }
         }
     }
 }
